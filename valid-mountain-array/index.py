49c128d9ffc45b27dee136ea4f35aabb81eebd2d
Jaan Paljasma <jpaljasma@gmail.com>
2024-01-10 09:17:30 -0500

    Fixed linting errors in valid-mountain-array

--- valid-mountain-array/index.py
+++ valid-mountain-array/index.py
@@ -1,13 +1,17 @@
+"""Typing to support List"""
 from typing import List
-"""
-Google Array Question: Valid Mountain Array (Easy)
-"""
 
 
 class Solution:
-    """See https://leetcode.com/problems/valid-mountain-array/description/"""
+    """
+    Google Array Question: Valid Mountain Array (Easy)
+    See https://leetcode.com/problems/valid-mountain-array/description/
+    """
 
-    def validMountainArray(self, arr: List[int]) -> bool:
+    def valid_mountain_array(self, arr: List[int]) -> bool:
+        """
+        Given an array of integers arr, return true if and only if it is a valid mountain array.
+        """
 
         # passes, 151ms runtime, 18.68 MB memory, beats 98.51% in runtime, 25.26% with memory in PY
         # space complexity O(1), we use only couple of variables
@@ -18,7 +22,8 @@ class Solution:
         # range checks
         if n_len < 3:
             return False
-        elif arr[1] <= arr[0]:
+
+        if arr[1] <= arr[0]:
             # immediate flat or fall
             return False
 
@@ -26,24 +31,33 @@ class Solution:
         true_mountain = False
 
         for i in range(1, n_len):
-            if arr[i] == arr[i-1]:
+            if arr[i] == arr[i - 1]:
                 # edge case, line is horizontal
                 true_mountain = False
                 break
-            elif raising and arr[i] < arr[i-1]:
+
+            if raising and arr[i] < arr[i - 1]:
                 # start falling
                 raising = False
                 true_mountain = True
-            elif not raising and arr[i] > arr[i-1]:
+            elif not raising and arr[i] > arr[i - 1]:
                 # was suppose to fall, but changed direction
                 true_mountain = False
                 break
         return true_mountain
 
+    def __repr__(self) -> str:
+        return "" + __class__
+
+
 # Test Runner
 
 
 class ValidMountainAreaTest:
+    """
+    Valid Mountain Area Test Runner
+    """
+
     def run(self, arr: List[int], expected: int) -> bool:
         """Runs the test case against Solution()
 
@@ -58,16 +72,19 @@ class ValidMountainAreaTest:
         Exception: If the unit test does not pass
         """
         s = Solution()
-        result = s.validMountainArray(arr)
+        result = s.valid_mountain_array(arr)
         if result == expected:
             print(f"Valid area is {result} for input {arr}")
             return True
-        else:
-            sx = f"Test failed, {result} != {expected} in {arr}"
-            raise Exception(sx)
+
+        sx = f"Test failed, {result} != {expected} in {arr}"
+        raise AssertionError(sx)
+
+    def __repr__(self) -> str:
+        return "" + __class__
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test = ValidMountainAreaTest()
     test.run([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], False)
     test.run([2, 1], False)
